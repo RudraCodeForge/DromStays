1431c2fc387f4002da2437a3abb82de4dcde3807
RudraCodeForge <jitandradaksh533@gmail.com>
2026-08-12 19:28:25 +0530

    feat: add persistent cart using redux-persist

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,20 +2,23 @@ import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./styles/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import App from "./App.jsx";
-import { store } from "./redux/Store";
+import { store, persistor } from "./redux/Store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </StrictMode>
+  </StrictMode>,
 );
--- Frontend/src/redux/Store.js
+++ Frontend/src/redux/Store.js
@@ -6,14 +6,41 @@ import ownerReducer from "./ownerSlice";
 import notificationReducer from "./notificationSlice";
 import cartReducer from "./cartSlice";
 
+import { persistStore, persistReducer } from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+
+const cartPersistConfig = {
+  key: "cart",
+  storage,
+};
+
+const cartPersistedReducer = persistReducer(cartPersistConfig, cartReducer);
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     notifications: notificationReducer,
     partner: partnerReducer,
     owner: ownerReducer,
-    cart: cartReducer,
+    cart: cartPersistedReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          "persist/PERSIST",
+          "persist/REHYDRATE",
+          "persist/PAUSE",
+          "persist/PURGE",
+          "persist/REGISTER",
+          "persist/FLUSH",
+        ],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export default store;
